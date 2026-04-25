2cfdb298f883cc4fd60d67c50a7067d0a970c60d
Ayush <myselfayush0536@gmail.com>
2026-04-25 18:38:57 +0530

    Refactor Booking model and add timestamps

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,23 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const bookingSchema = new mongoose.Schema({
-//   customer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },//customer later
-//   provider: { type: mongoose.Schema.Types.ObjectId, ref: 'Provider', required: true },
-//   service: { type: String, required: true },
-//   date: { type: Date, required: true },
-//   time: { type: String, required: true },
-//   status: { 
-//     type: String, 
-//     enum: ['pending', 'confirmed', 'cancelled', 'completed'], 
-//     default: 'pending' 
-//   },
-//   customerPhone: { type: String, required: true },
-//   createdAt: { type: Date, default: Date.now }
-// });
-
-// module.exports = mongoose.model('Booking', bookingSchema,"BOOKINGS");
-
-// models/Booking.js
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
